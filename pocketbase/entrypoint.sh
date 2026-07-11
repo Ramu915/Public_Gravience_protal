89948061d0a2ecc080f06eabb75fe557b02c5be8
Ramu <ramramurs915@gmil.com>
2026-07-11 19:23:53 +0530

    changed color

--- pocketbase/entrypoint.sh
+++ pocketbase/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 set -eu
+
 PORT="${PORT:-8080}"
-exec /pb/pocketbase serve --http=0.0.0.0:${PORT} --dir=/pb/pb_data --migrationsDir=/pb/pb_migrations
+
+exec /pb/pocketbase serve \
+  --http=0.0.0.0:${PORT} \
+  --dir=/pb/pb_data \
+  --migrationsDir=/pb/pb_migrations
